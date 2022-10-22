732af4e9ced373468acc4c727f61f1b17b5bb2ae
João <joaopedro.lima@al.infnet.edu.br>
2022-10-22 08:14:16 -0300

    Project generated from Spring.io

---
+++ AppOrderProduct/src/main/java/br/edu/infnet/AppOrderProduct/AppOrderProductApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.AppOrderProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOrderProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppOrderProductApplication.class, args);
+	}
+
+}
---
+++ AppOrderProduct/src/test/java/br/edu/infnet/AppOrderProduct/AppOrderProductApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.AppOrderProduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppOrderProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
