12560a37f60044b383af21cb59ab2f75fb615785
João <joaopedro.lima@al.infnet.edu.br>
2022-11-13 10:04:05 -0300

    FEATURE 4

---
+++ api-account/src/test/java/br/edu/infnet/apiaccount/ApiAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.apiaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
